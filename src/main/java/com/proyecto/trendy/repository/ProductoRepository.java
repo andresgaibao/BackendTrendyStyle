76e10797f7002e00bf5ad580ffea502acb8bef09
Andres <gaibaoandres@gmail.com>
2024-02-27 16:44:26 -0500

    Inicial Commit

---
+++ src/main/java/com/proyecto/trendy/repository/ProductoRepository.java
@@ -0,0 +1,9 @@
+package com.proyecto.trendy.repository;
+
+import com.proyecto.trendy.entity.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+}
