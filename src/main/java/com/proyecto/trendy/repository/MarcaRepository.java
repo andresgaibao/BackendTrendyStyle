db7e59b3d533d830d7356d3893498e5d7275e5a1
Andres <gaibaoandres@gmail.com>
2024-02-29 11:31:23 -0500

    Cambios

--- src/main/java/com/proyecto/trendy/repository/MarcaRepository.java
+++ src/main/java/com/proyecto/trendy/repository/MarcaRepository.java
@@ -1,6 +1,5 @@
 package com.proyecto.trendy.repository;
 
-import com.proyecto.trendy.entity.Marca;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/proyecto/trendy/repository/ProductoRepository.java
+++ src/main/java/com/proyecto/trendy/repository/ProductoRepository.java
@@ -1,6 +1,5 @@
 package com.proyecto.trendy.repository;
 
-import com.proyecto.trendy.entity.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
