ec127e64230ba7b059ab27732c7273646275aa27
Andres <gaibaoandres84@gmail.com>
2024-03-12 20:52:39 -0500

    UserInfo cambios

---
+++ src/main/java/com/proyecto/trendy/repository/DetailRepository.java
@@ -0,0 +1,9 @@
+package com.proyecto.trendy.repository;
+
+import com.proyecto.trendy.entity.Detail;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DetailRepository extends JpaRepository<Detail, Integer> {
+}
